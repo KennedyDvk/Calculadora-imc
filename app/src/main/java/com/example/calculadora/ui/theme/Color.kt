18a4b9e66dec8a2ac2bd7e7e06efae8fd71cb6cc
Kennedy Cesar Pimentel <104803647+KennedyDvk@users.noreply.github.com>
2023-08-26 12:50:01 -0300

    Apenas commit

---
+++ app/src/main/java/com/example/calculadora/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.calculadora.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFED145B)
+val PurpleGrey80 = Color(0xFF111111)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFFED145B)
+val PurpleGrey40 = Color(0xFF4B494E)
+val Pink40 = Color(0xFF7D5260)
